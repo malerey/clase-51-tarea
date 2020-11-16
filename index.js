edfbb3d6fa18dfc6a83298058269fb19e259cd9d
Malena Rey <36973939+malerey@users.noreply.github.com>
2020-11-16 20:24:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const cuadrado1 = document.querySelector(`div[data-x='0'][data-y='0']`)
+const cuadrado2 = document.querySelector(`div[data-x='0'][data-y='1']`)
+const cuadrado3 = document.querySelector(`div[data-x='1'][data-y='0']`)
+const cuadrado4 = document.querySelector(`div[data-x='1'][data-y='1']`)
